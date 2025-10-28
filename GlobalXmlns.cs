f60e1ed890df495b9c03e72deaac84b8384c4d9c
AndresSimba <wilo.andres14@gmail.com>
2025-10-28 00:42:30 -0500

    Agregar archivos de proyecto.

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ParkIsrael_Octavo")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ParkIsrael_Octavo.Pages")]
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ParkIsrael_Octavo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
