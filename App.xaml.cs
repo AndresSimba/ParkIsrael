05171f01081d4f875131572701e288d5eac2a614
Elvin Jesús Sánchez <laden1786@ymail.com>
2025-11-23 09:28:23 -0500

    revision con wsimbana

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,7 +9,7 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new Views.vDatos());
+            return new Window(new NavigationPage (new Views.vLogin()));
         }
     }
 }
